e51da45ed71faaa5db13f17e4d73ef4b05f2e148
joaomalfatti <joao.cursos1210@gmail.com>
2024-09-02 21:54:06 -0300

    Foi criado pastas para separação de cada rota

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,19 @@
 module.exports = {
-  // outras configurações
-  files: ['**/*.ts'],
-  // plugins, rules, etc.
+  // Outras configurações globais
+
+  overrides: [
+    {
+      files: ['**/*.{js,ts,tsx}'],
+      parser: '@typescript-eslint/parser',
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      rules: {
+        // Suas regras específicas para arquivos .js, .ts e .tsx
+      },
+    },
+  ],
+  
+  // Outros plugins, rules, etc.
 };
