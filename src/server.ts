09b2b147ef4a0eff2ce2e1ddc08fc5c916db757e
joaomalfatti <joao.cursos1210@gmail.com>
2024-09-05 20:23:40 -0300

    Foi criado um token jwt

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 
 import fastify from "fastify";
 import cors from '@fastify/cors';
+import  jwt  from '@fastify/jwt';
 import {memoriesRoutes} from './routes/memories'
 import { authRoutes } from './routes/auth';
 
@@ -10,6 +11,10 @@ const app = fastify()
 
 app.register(cors, {
   origin: true, //Todas URLs de front-end poderão acessar o nosso back-end
+})
+
+app.register(jwt, {
+  secret: 'spacetime', //Maneira de diferenciar os tokens de outros jwt outros back-end
 })
 
 //Registrar um arquivo de rota separados.
